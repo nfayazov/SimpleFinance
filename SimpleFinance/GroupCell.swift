872f4046dcd9cde2d006d3137562c3c63dfbbe26
Nadir Fayazov <nfayazov@gmail.com>
2016-12-05 19:13:56 -0800

    Added Total label, delete group warning and more

--- SimpleFinance/GroupCell.swift
+++ SimpleFinance/GroupCell.swift
@@ -16,19 +16,34 @@ class GroupCell: UITableViewCell {
     label.translatesAutoresizingMaskIntoConstraints = false
     label.font = UIFont(name: "Helvetica Neue", size: 15)
     return label
-    }() 
+    }()
+    
+    let colorLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: "groupCell")
         
+        addSubview(colorLabel)
         addSubview(progressLabel)
-        
+       
         progressLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         progressLabel.centerYAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
         progressLabel.widthAnchor.constraint(equalToConstant: 120).isActive = true
         progressLabel.heightAnchor.constraint(equalTo: (textLabel?.heightAnchor)!).isActive = true
         progressLabel.textColor = UIColor.lightGray
         
+        colorLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        colorLabel.centerYAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        colorLabel.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        colorLabel.heightAnchor.constraint(equalTo: (textLabel?.heightAnchor)!).isActive = true
+        colorLabel.backgroundColor = UIColor.yellow
+        colorLabel.alpha = 0.4
         
     }
 
